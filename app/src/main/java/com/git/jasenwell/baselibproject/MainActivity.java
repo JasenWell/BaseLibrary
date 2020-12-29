0f292e4b613f1d2c536433b1c7c48c84be7f2307
unknown <409762605@qq.com>
2020-12-29 21:57:26 +0800

    test by hjh

--- app/src/main/java/com/git/jasenwell/baselibproject/MainActivity.java
+++ app/src/main/java/com/git/jasenwell/baselibproject/MainActivity.java
@@ -6,6 +6,10 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 import com.hjh.baselib.base.LBaseActivity;
+import com.hjh.baselib.file.FileScaner;
+import com.hjh.baselib.file.ImageFileDirectory;
+
+import java.util.List;
 
 import butterknife.BindView;
 
@@ -22,6 +26,19 @@ public class MainActivity extends LBaseActivity {
     @Override
     public void onLoadDefaultData() {
         super.onLoadDefaultData();
-        textView.setText("Hello World! baselib");
+        textView.setText("Hello World! baselib test2");
+        checkCameraPermission();
+    }
+
+    @Override
+    public boolean showPermissionsResult(boolean result) {
+        return false;
+    }
+
+    @Override
+    public void onResponsePermissionsResult(boolean result) {
+        if(result){
+            List<ImageFileDirectory>  list = FileScaner.getInstance().scanImageFile(this,true);
+        }
     }
 }
